5948da11afa68f6ea3ef8c67e3a5013baca98de7
Ciprian Chesa <chesa_ciprian@yahoo.com>
2021-03-04 22:00:15 +0200

    add team UI

---
+++ functions.js
@@ -0,0 +1,49 @@
+let allTeams = [];
+
+function getHtmlTeams(teams) {
+    return teams.map(team => {
+        return `<tr>
+            <td>${team.members}</td>
+            <td>${team.name}</td>
+            <td>${team.url}</td>
+        </tr>`
+    }).join("")
+}
+
+function showTeams(teams) {
+    const html = getHtmlTeams(teams);
+    const tbody = document.querySelector("tbody");
+    tbody.innerHTML = html;
+}
+
+fetch("teams.json")
+    .then(r => r.json())
+    .then(teams => {
+        allTeams = teams;
+        showTeams(teams);
+    });
+
+function addTeam(team) {
+        fetch("add.json", {
+            method: "POST",
+            body: JSON.stringify(team)
+        })
+        .then(r => r.json())
+        .then(status => {
+            console.warn("status", status);
+        });
+}
+
+function saveTeam() {
+    const members =  document.querySelector("input[name=members]").value;
+    const name =  document.querySelector("input[name=name]").value;
+    const url =  document.querySelector("input[name=url]").value;
+
+    const team = {
+        name: name,
+        members: members,
+        url: url
+    };
+    
+    addTeam(team);
+}
